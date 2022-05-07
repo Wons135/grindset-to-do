7737cdd7f9469f754517301b973892119115099a
Łukasz <33837810+Wons135@users.noreply.github.com>
2022-05-07 14:36:18 +0200

    Added templates and template inheritance.

--- grindset/pages/views.py
+++ grindset/pages/views.py
@@ -3,8 +3,11 @@ from django.http import HttpResponse
 
 # Create your views here.
 
-def home_view(*args, **kwargs):
-    return HttpResponse('<h1>Good start</h1>')
+def home_view(request, *args, **kwargs):
+    print(request.user)
+    #return HttpResponse('<h1>Good start</h1>')
+    return render(request, 'pages/home.html', {})
 
-def contact_view(*args, **kwargs):
-    return HttpResponse('<h1>Contact Page</h1>')
+def contact_view(request, *args, **kwargs):
+    print(request)
+    return render(request, 'pages/contact.html', {})
